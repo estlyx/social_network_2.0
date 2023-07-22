0f533b2b5cc96fce819202fed62f02ed845458be
estlyx <koshelyuk-03@mail.ru>
2023-07-22 13:35:22 +0300

    Initial commit

---
+++ social_network_2/social_network_2/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', user_views.my_profile, name='home'),
+    path('users/<slug:slug>/', user_views.profile_view, name='profile_view'),
+    path('friends/', user_views.friend_list, name='friend_list'),
+    path('users/friend-request/send/<int:id>/', user_views.send_friend_request, name='send_friend_request'),
+    path('users/friend-request/cancel/<int:id>/', user_views.cancel_friend_request, name='cancel_friend_request'),
+    path('users/friend-request/accept/<int:id>/', user_views.accept_friend_request, name='accept_friend_request'),
+    path('users/friend-request/delete/<int:id>/', user_views.delete_friend_request, name='delete_friend_request'),
+    path('users/friend/delete/<int:id>/', user_views.delete_friend, name='delete_friend'),
+    path('edit-profile/', user_views.edit_profile, name='edit_profile'),
+    path('my-profile/', user_views.my_profile, name='my_profile'),
+    path('search_users/', user_views.search_users, name='search_users'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]
